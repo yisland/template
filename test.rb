9982da26a1522cb51b4885401bc5f968522fdb12
yisland <yisland@yisland-no-MacBook-Air.local>
2014-05-15 05:51:14 +0900

    空白削除Case追加

--- test.rb
+++ test.rb
@@ -9,6 +9,7 @@ REPARRAY = TEMPLATE["default"]["replace"]
 TMPFILENAME = TEMPLATE["default"]["csvFileName"]
 OUTFILENAME = TEMPLATE["default"]["outputFileName"]
 LOWORUPCASE = TEMPLATE["default"]["lowOrUpCase"]
+STRIPCASE = TEMPLATE["default"]["stripCase"]
 
 outArray = []
 
@@ -16,11 +17,16 @@ CSV.foreach(TMPFILENAME, { :encoding => "UTF-8"}) do |row|
     i = 0
     tmp = TMPVAL
     row.each do |column|
+        # 大文字・小文字変換
         if LOWORUPCASE == "lower" then
             column.downcase!
         elsif LOWORUPCASE == "up" then
             column.upcase!
         end
+
+        # 空白削除
+        column.strip! if STRIPCASE
+
         tmp = tmp.gsub(REPARRAY[i], column)
         i += 1
     end
